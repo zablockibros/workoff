aeeabf22d082b60e964fd2444afc10ef4c351bce
Justin Zablocki <justin@quizzle.com>
2016-03-04 16:17:25 -0500

    Add domain fetching routes

--- controllers/api/v1/api.js
+++ controllers/api/v1/api.js
@@ -4,6 +4,7 @@ var passport = require('passport');
 var passportConfig = require('../../../config/passport');
 var User = require('../../../models/User');
 var Post = require('../../../models/Post');
+var Domain = require('../../../models/Domain');
 var UserManager = require('../../../models/managers/UserManager');
 var PostManager = require('../../../models/managers/PostManager');
 var Activity = require('../../../models/Activity');
@@ -35,6 +36,18 @@ router.param('comment', function(req, res, next, id) {
       next();
     } else {
       next(new Error('Could not find that comment'));
+    }
+  });
+});
+router.param('domain', function(req, res, next, id) {
+  Domain.findById(id, function(err, domain) {
+    if (err) {
+      next(err);
+    } else if (domain) {
+      req.domain = domain;
+      next();
+    } else {
+      next(new Error('Could not find that domain'));
     }
   });
 });
@@ -160,6 +173,113 @@ router.post('/logout', function(req, res) {
 });
 
 
+// DOMAIN ROUTES
+
+/**
+ * GET /domains
+ * Get a list of all domains
+ */
+router.get('/domains', function(req, res) {
+  if (!req.user) {
+    return res.status(401).json({ error: 'You are not logged in!' });
+  }
+
+  Domain
+    .find()
+    .exec(function(err, domains) {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      res.json(domains);
+    });
+});
+
+/**
+ * GET /domain/:domain
+ * Get a domain by ID
+ */
+router.get('/domain/:domain', function(req, res) {
+  if (!req.user) {
+    return res.status(401).json({ error: 'You are not logged in!' });
+  }
+
+  res.json(req.domain);
+});
+
+/**
+ * GET /domains?name=
+ * Search for a domain
+ */
+router.get('/domains', function(req, res) {
+  if (!req.user) {
+    return res.status(401).json({ error: 'You are not logged in!' });
+  }
+    if (!req.user) {
+      return res.status(401).json({ error: 'You are not logged in!' });
+    }
+
+    req.assert('name', 'Please provide a domain name to find').notEmpty();
+    req.sanitize('name').escape();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+      return res.status(400).json(errors);
+    }
+
+    Domain
+      .find({
+        name: req.query.name
+      })
+      .exec(function(err, domains) {
+        if (err) {
+          return res.status(400).json({ error: err });
+        }
+        res.json(domains);
+      });
+});
+
+/**
+ * GET /domain/:domain/users
+ * Get the domain users list
+ */
+router.get('/domain/:domain/users', function(req, res) {
+  if (!req.user) {
+    return res.status(401).json({ error: 'You are not logged in!' });
+  }
+
+  User
+    .find({
+      domain: req.domain
+    })
+    .select({
+      email: 1,
+      profile: 1
+    })
+    .exec(function(err, users) {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      res.json({
+        users: users,
+        domain: req.domain
+      })
+    })
+});
+
+/**
+ * POST /invite
+ * Invite a user via email
+ */
+router.post('/invite', function(req, res) {
+  if (!req.user) {
+    return res.status(401).json({ error: 'You are not logged in!' });
+  }
+
+  res.json({});
+});
+
+
 // POST ROUTES
 
 /**
@@ -169,7 +289,6 @@ router.post('/logout', function(req, res) {
 router.get('/home', function(req, res) {
   res.json({ user: { name: "God" } });
 });
-
 
 /**
  * POST /post
