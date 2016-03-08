4a92821d4479ed4e104bd91f1ba420b45de10580
Justin Zablocki <justin@quizzle.com>
2016-03-08 16:26:32 -0500

    Add webpack

---
+++ models/managers/NotifyManager.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var Notification = require('../Notification');
+var Q = require('q');
+
+/**
+ * Builds a basic notification object
+ */
+exports.buildNotification = function() {
+  var notif = new Notification({});
+};
